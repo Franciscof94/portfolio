e152e999fa77092ab55f23f90b97ab8b3c36068c
francisco <fran94_ferraro@hotmail.com>
2022-06-22 15:23:24 -0300

    styles changed

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,93 +1,99 @@
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, makeStyles, Typography } from '@material-ui/core';
-import { useState } from 'react';
-import BasicModal from './BasicModal';
-import Line from './Line';
-import data from '../data/data'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import Line from "./Line";
+import data from "../data/data";
 
 const Portfolio = () => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const[filterArray, setFilterArray] = useState()
-
-  
-
 
   return (
     <div>
       <h1 className={classes.title}>Portafolio</h1>
-      <h3 className={classes.projects}>Mis <span className={classes.span}>proyectos</span></h3>
+      <h3 className={classes.projects}>
+        Mis <span className={classes.span}>proyectos</span>
+      </h3>
       <div className={classes.container}>
+        {data.map((img, i) => (
+          <Card className={classes.root}>
+            <CardActionArea>
+            <a href={img.link} target="_blank" rel="noopener noreferrer" className={classes.a}>
+              <CardMedia
+                component="img"
+                alt="Contemplative Reptile"
+                height="140"
+                image={img.url}
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2" className={classes.projectTitle}>
+                  {img.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p" className="description">
+                  {img.description}
+                </Typography>
+              </CardContent>
+              </a>
+            </CardActionArea>
+          </Card>
+        ))}
+      </div>
 
-        {
-          data.map((img, i) => (
-            <Card sx={{ maxWidth: 155 }} key={img.alt} className={classes.card}> 
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    className={classes.cardMedia}
-                    image={img.url}
-                    alt={img.alt}
-                    onClick={() => {
-                      handleOpen();
-                      setFilterArray(i);
-                    }}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div" className={classes.name}>
-                      {img.title}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-          ))
-          }
-        </div>
-        <BasicModal open={open} 
-                    handleClose={handleClose}
-                    filterArray={filterArray}
-        />
-        <Line />
+      <Line />
     </div>
-  )
-}
+  );
+};
 
 const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
     justifyContent: "space-evenly",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
   },
   card: {
     width: "38vh",
     margin: "7px",
-    [theme.breakpoints.down("sm")] : {
-      width: "20vh"
+    [theme.breakpoints.down("sm")]: {
+      width: "20vh",
     },
   },
-  
+  a: {
+    textDecoration: "none"
+  },
   cardMedia: {
-    height: "18vh"
+    height: "18vh",
   },
   skills: {
-    fontSize: "1.2rem"
+    fontSize: "1.2rem",
   },
   name: {
     fontSize: "1rem",
-    color: "#202020"
+    color: "#202020",
   },
   title: {
     fontSize: "3rem",
-    color: "#202020"
+    color: "#202020",
   },
   projects: {
     fontSize: "2rem",
-    color: "#202020"
+    color: "#202020",
   },
   span: {
-    color: "#3586FF"
+    color: "#3586FF",
+  },
+  root: {
+    maxWidth: 380,
+    margin: "15px"
+  },
+  projectTitle: {
+    color: "#202020",
+    fontWeight: "bold"
   }
-}))
+}));
 
-export default Portfolio
+export default Portfolio;
--- src/data/data.js
+++ src/data/data.js
@@ -1,191 +1,218 @@
-import Drinks from '../Images/drinks.png';
-import Pinasco from '../Images/pinasco.png';
-import Tinder from '../Images/tinder.png';
-import Netflix from '../Images/netflix.png';
-import Instagram from '../Images/instagram.png';
-import Tienda from '../Images/tienda.png';
-import Logifer from '../Images/logifer.png';
+import Drinks from "../Images/drinks.png";
+import Pinasco from "../Images/pinasco.png";
+import Tinder from "../Images/tinder.png";
+import Netflix from "../Images/netflix.png";
+import Instagram from "../Images/instagram.png";
+import Tienda from "../Images/tienda.png";
+import Logifer from "../Images/logifer.png";
+import GoScrum from "../Images/goscrum.png";
 import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5";
 import { FaCss3Alt } from "@react-icons/all-files/fa/FaCss3Alt";
 import { FaReact } from "@react-icons/all-files/fa/FaReact";
 import { FaNode } from "@react-icons/all-files/fa/FaNode";
-import { SiRedux } from 'react-icons/si';
-import { SiJavascript } from 'react-icons/si';
-import { SiFirebase } from 'react-icons/si';
-import { SiMaterialui } from 'react-icons/si'
-import { SiStyledcomponents } from 'react-icons/si'
-
-
+import { SiRedux } from "react-icons/si";
+import { SiJavascript } from "react-icons/si";
+import { SiFirebase } from "react-icons/si";
+import { SiMaterialui } from "react-icons/si";
+import { SiStyledcomponents } from "react-icons/si";
 
 const img = [
-    {
-      id: 0,
-      alt: 'Drinks',
-      url: `${Drinks}`,
-      title: 'E-commerce de bebidas',
-      icons : [
+  {
+    id: 0,
+    alt: "App de tareas - Alkemy",
+    url: `${GoScrum}`,
+    title: "App de tareas - Alkemy",
+    icons: [
       {
-        icon: <FaHtml5 fontSize='32' color='#DD4B25' />,
-        name: 'HTML'
+        icon: <FaReact fontSize="32" color="#00DBF7" />,
+        name: "React Js",
       },
       {
-        icon: <FaCss3Alt fontSize='32' color='#2173F7' />,
-        name: 'CSS'
+        icon: <SiStyledcomponents fontSize="32" color="#f5cd66" />,
+        name: "Styled Components",
       },
       {
-        icon: <SiJavascript fontSize='32' color='#EFD81D' />,
-        name: 'JavaScript'
-      }
-      ], 
-      description: 'Tienda de bebidas - Proyecto final para el curso de Java Script en Coder House',
-      link: 'https://franciscof94.github.io/E-commerce/index.html'
-    },
-    {
-      id: 1,
-      alt: 'Pinasco',
-      url: `${Pinasco}`,
-      title: 'Maximo Pinasco',
-      icons : [
-          {
-            icon: <FaHtml5 fontSize='32' color='#DD4B25' />,
-            name: 'HTML'
-          },
-          {
-            icon: <FaCss3Alt fontSize='32' color='#2173F7' />,
-            name: 'CSS'
-          },
-          {
-            icon: <SiJavascript fontSize='32' color='#EFD81D' />,
-            name: 'JavaScript'
-          },
-          {
-            icon: <FaNode fontSize='32' color='#313131' />,
-            name: 'Node Express'
-          }
-      ],
-      description: 'Pagina Web (en proceso) para el concesionario de autos Maximo Pinasco, desarrollada en HTML, CSS y JavaScript, aparte utilice Node Express y Nodemailer para el envio de formularios.',
-      link: ''
-    },
-    {
-      id: 2,
-      alt: 'Tinder',
-      url: `${Tinder}`,
-      title: 'Clon de Tinder',
-      icons : [
-        {
-          icon: <FaReact fontSize='32' color='#00DBF7' />,
-          name: 'React Js'
-        },
-        {
-          icon: <SiMaterialui fontSize='32' color='#00AAF7' />,
-          name: 'Materal UI'
-        },
-        {
-          icon: <SiFirebase fontSize='32' color='#F7C52B' />,
-          name: 'Firebase'
-        },
-        {
-          icon: <SiRedux fontSize='32' color='#7248B5' />,
-          name: 'Redux'
-        }
-        ],
-      description: 'Clon de Tinder desarrollado en React Js, utilizando tambien Firebase, Redux y Material UI',
-      link: 'https://clone-tinder-simpsons.netlify.app/' 
-    },
-    {
-      id: 3,
-      alt: 'Netflix',
-      url: `${Netflix}`,
-      title: 'Clon de Netflix',
-      icons : [
-        {
-          icon: <FaReact fontSize='32' color='#00DBF7' />,
-          name: 'React Js'
-        },
-        {
-          icon: <SiMaterialui fontSize='32' color='#00AAF7' />,
-          name: 'Materal UI'
-        },
-        {
-          icon: <SiFirebase fontSize='32' color='#F7C52B' />,
-          name: 'Firebase'
-        },
-        {
-          icon: <SiRedux fontSize='32' color='#7248B5' />,
-          name: 'Redux'
-        }
-        ],
-      description: 'Clon de Netflix desarrollado en React Js, utilizando tambien Firebase, Redux y Material UI',
-      link: 'https://nifty-bhabha-c18f88.netlify.app/'
-    },
-    {
-      id: 4,
-      alt: 'Instagram',
-      url: `${Instagram}`,
-      title: 'Clon de Instagram',
-      icons : [
-        {
-          icon: <FaReact fontSize='32' color='#00DBF7' />,
-          name: 'React Js'
-        },
-        {
-          icon: <SiStyledcomponents fontSize='32' color='#f5cd66' />,
-          name: 'Styled Components'
-        },
-        {
-          icon: <SiFirebase fontSize='32' color='#F7C52B' />,
-          name: 'Firebase'
-        },
-        ],
-      description: 'Clon de Instagram desarrollado en React Js, utilizando Styled Components para los estilos y Firebase para almacenar datos.',
-      link: 'https://cloninstagram.netlify.app/'
-    },
-    {
-      id: 5,
-      alt: 'Tienda de productos(API Fake-Store)',
-      url: `${Tienda}`,
-      title: 'Tienda de productos(API Fake-store)',
-      icons : [
-        {
-          icon: <FaReact fontSize='32' color='#00DBF7' />,
-          name: 'React Js'
-        },
-        {
-          icon: <SiStyledcomponents fontSize='32' color='#f5cd66' />,
-          name: 'Styled Components'
-        },
-        {
-          icon: <SiRedux fontSize='32' color='#7248B5' />,
-          name: 'Redux'
-        },
-        ],
-      description: 'Tienda de productos desarrollada en React Js con redux(filtrar productos, agregar productos al carrito, eliminarlosy contabilizarlos) y styled components para los estilos.',
-      link: 'https://franciscof94.github.io/fake-store-react-redux'
-    },
-    {
-      id: 6,
-      alt: 'Web Ferraro Materiales',
-      url: `${Logifer}`,
-      title: 'Web Ferraro Materiales',
-      icons : [
-        {
-          icon: <FaReact fontSize='32' color='#00DBF7' />,
-          name: 'React Js'
-        },
-        {
-          icon: <SiMaterialui fontSize='32' color='#00AAF7' />,
-          name: 'Materal UI'
-        },
-        {
-          icon: <SiRedux fontSize="32" color="#7248B5" />,
-          name: 'Redux'
-        },
-        ],
-      description: 'Pagina web de Ferraro Materiales desarrollada en React js con redux(filtrado de productos) y styled components para los estilos.',
-      link: 'https://ferraromateriales.com.ar/'
-    }
-  ]
+        icon: <SiRedux fontSize="32" color="#7248B5" />,
+        name: "Redux",
+      },
+    ],
+    description:
+      "App de tareas GoScrum - Alkemy desarrollada en React js con redux(filtrar y editar tareas) y styled components para los estilos.",
+    link: "https://go-scrum-alkemy.vercel.app/",
+  },
+  {
+    id: 1,
+    alt: "Web Ferraro Materiales",
+    url: `${Logifer}`,
+    title: "Web Ferraro Materiales",
+    icons: [
+      {
+        icon: <FaReact fontSize="32" color="#00DBF7" />,
+        name: "React Js",
+      },
+      {
+        icon: <SiMaterialui fontSize="32" color="#00AAF7" />,
+        name: "Materal UI",
+      },
+      {
+        icon: <SiRedux fontSize="32" color="#7248B5" />,
+        name: "Redux",
+      },
+    ],
+    description:
+      "Pagina web de Ferraro Materiales desarrollada en React js con redux(filtrado de productos) y styled components para los estilos.",
+    link: "https://ferraromateriales.com.ar/",
+  },
+  {
+    id: 2,
+    alt: "Tienda de productos(API Fake-Store)",
+    url: `${Tienda}`,
+    title: "Tienda de productos(API Fake-store)",
+    icons: [
+      {
+        icon: <FaReact fontSize="32" color="#00DBF7" />,
+        name: "React Js",
+      },
+      {
+        icon: <SiStyledcomponents fontSize="32" color="#f5cd66" />,
+        name: "Styled Components",
+      },
+      {
+        icon: <SiRedux fontSize="32" color="#7248B5" />,
+        name: "Redux",
+      },
+    ],
+    description:
+      "Tienda de productos desarrollada en React Js con redux(filtrar productos, agregar productos al carrito, eliminarlosy contabilizarlos) y styled components para los estilos.",
+    link: "https://franciscof94.github.io/fake-store-react-redux",
+  },
+  {
+    id: 3,
+    alt: "Pinasco",
+    url: `${Pinasco}`,
+    title: "Maximo Pinasco",
+    icons: [
+      {
+        icon: <FaHtml5 fontSize="32" color="#DD4B25" />,
+        name: "HTML",
+      },
+      {
+        icon: <FaCss3Alt fontSize="32" color="#2173F7" />,
+        name: "CSS",
+      },
+      {
+        icon: <SiJavascript fontSize="32" color="#EFD81D" />,
+        name: "JavaScript",
+      },
+      {
+        icon: <FaNode fontSize="32" color="#313131" />,
+        name: "Node Express",
+      },
+    ],
+    description:
+      "Web para el concesionario de autos Maximo Pinasco desarrollada en HTML, CSS y JavaScript, aparte utilicé Node Express junto con nodemailer para el envio de formularios y MySQL para almacenar datos de usuarios que deseen recibir información adicional.",
+    link: "https://maximopinascosa.com.ar/",
+  },
+  {
+    id: 4,
+    alt: "Netflix",
+    url: `${Netflix}`,
+    title: "Clon de Netflix",
+    icons: [
+      {
+        icon: <FaReact fontSize="32" color="#00DBF7" />,
+        name: "React Js",
+      },
+      {
+        icon: <SiMaterialui fontSize="32" color="#00AAF7" />,
+        name: "Materal UI",
+      },
+      {
+        icon: <SiFirebase fontSize="32" color="#F7C52B" />,
+        name: "Firebase",
+      },
+      {
+        icon: <SiRedux fontSize="32" color="#7248B5" />,
+        name: "Redux",
+      },
+    ],
+    description:
+      "Clon de Netflix desarrollado en React Js, utilizando tambien Firebase, Redux y Material UI",
+    link: "https://nifty-bhabha-c18f88.netlify.app/",
+  },
+  {
+    id: 5,
+    alt: "Tinder",
+    url: `${Tinder}`,
+    title: "Clon de Tinder",
+    icons: [
+      {
+        icon: <FaReact fontSize="32" color="#00DBF7" />,
+        name: "React Js",
+      },
+      {
+        icon: <SiMaterialui fontSize="32" color="#00AAF7" />,
+        name: "Materal UI",
+      },
+      {
+        icon: <SiFirebase fontSize="32" color="#F7C52B" />,
+        name: "Firebase",
+      },
+      {
+        icon: <SiRedux fontSize="32" color="#7248B5" />,
+        name: "Redux",
+      },
+    ],
+    description:
+      "Clon de Tinder desarrollado en React Js, utilizando tambien Firebase, Redux y Material UI",
+    link: "https://clone-tinder-simpsons.netlify.app/",
+  },
+  {
+    id: 6,
+    alt: "Instagram",
+    url: `${Instagram}`,
+    title: "Clon de Instagram",
+    icons: [
+      {
+        icon: <FaReact fontSize="32" color="#00DBF7" />,
+        name: "React Js",
+      },
+      {
+        icon: <SiStyledcomponents fontSize="32" color="#f5cd66" />,
+        name: "Styled Components",
+      },
+      {
+        icon: <SiFirebase fontSize="32" color="#F7C52B" />,
+        name: "Firebase",
+      },
+    ],
+    description:
+      "Clon de Instagram desarrollado en React Js, utilizando Styled Components para los estilos y Firebase para almacenar datos.",
+    link: "https://cloninstagram.netlify.app/",
+  },
+  {
+    id: 7,
+    alt: "Drinks",
+    url: `${Drinks}`,
+    title: "E-commerce de bebidas",
+    icons: [
+      {
+        icon: <FaHtml5 fontSize="32" color="#DD4B25" />,
+        name: "HTML",
+      },
+      {
+        icon: <FaCss3Alt fontSize="32" color="#2173F7" />,
+        name: "CSS",
+      },
+      {
+        icon: <SiJavascript fontSize="32" color="#EFD81D" />,
+        name: "JavaScript",
+      },
+    ],
+    description:
+      "Tienda de bebidas - Proyecto final para el curso de Java Script en Coder House",
+    link: "https://franciscof94.github.io/E-commerce/index.html",
+  },
+];
 
-  
-
-  export default img
+export default img;
